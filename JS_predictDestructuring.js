6d9c63947608b8111b9034039063caae8a0ac9ee
weicheng lee <z82111712@gmail.com>
2022-01-07 22:56:14 -0800

    second attempt

--- JS_predictDestructuring.js
+++ JS_predictDestructuring.js
@@ -3,7 +3,9 @@ const cars = ['Tesla', 'Mercedes', 'Honda'];
 const [ randomCar ] = cars;
 const [ , otherRandomCar ] = cars;
 console.log(randomCar);
+// Tesla
 console.log(otherRandomCar);
+// Mercedes
 
 // 2
 const employee = {
@@ -13,7 +15,9 @@ const employee = {
 }
 const { name: otherName } = employee;
 console.log(name);
+// undefined
 console.log(otherName);
+// Elon
 
 // 3
 const person = {
@@ -24,7 +28,9 @@ const person = {
 const password = '12345';
 const { password: hashedPassword } = person;
 console.log(password);
+// undefined
 console.log(hashedPassword);
+// undefined
 
 // 4
 const numbers = [8,2,3,5,6,1,67,12,2];
@@ -32,7 +38,9 @@ const [, first] = numbers;
 const [,,,second] = numbers;
 const [,,,,,,,,third] = numbers;
 console.log(first === second);
+// false
 console.log(first === third);
+// true
 
 // 5
 const lastTest = {
@@ -43,6 +51,10 @@ const {key} = lastTest;
 const {secondKey} = lastTest;
 const [ , willThisWork] = secondKey;
 console.log(key)
+// value
 console.log(secondKey)
+// [1,5,1,8,3,3]
 console.log(secondKey[0])
+// 1
 console.log(willThisWork)
+// 5
